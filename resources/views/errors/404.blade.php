d723e34cb669afa2602b93bc1413f2fbe5a19e35
Pavel Janko <sainthrax@gmail.com>
2017-12-11 17:39:33 +0100

    Added an error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.basic')
+
+@section('content')
+    <div class="d-flex flex-column align-items-center mt-4">
+        <h1 class="display-3 mb-4">Stránku se nepodařilo nalézt</h1>
+        <h1 class="display-4 mb-4">Tady máte mem <i class="fal fa-smile"></i></h1>
+        <div>
+            <img class="img-fluid" src="https://i.imgur.com/wgvFzU5.png" alt="Dank meme">
+        </div>
+    </div>
+@endsection
